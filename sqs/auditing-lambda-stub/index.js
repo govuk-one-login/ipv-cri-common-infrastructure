331dd3cfc13b69254ecae66c1847c513c4f29fb2
Tim Preece <19146299+mrdoodles@users.noreply.github.com>
2022-04-29 17:07:37 +0100

    KBV-354 remove stub function and related configuration

--- sqs/auditing-lambda-stub/index.js
+++
@@ -1,9 +0,0 @@
-// Taken from https://docs.aws.amazon.com/lambda/latest/dg/with-sqs-create-package.html#with-sqs-example-deployment-pkg-nodejs
-// Will read events from an SQS queue and log them
-exports.handler = async function(event, context) {
-    event.Records.forEach(record => {
-        const { body } = record;
-        console.log(body);
-    });
-    return {};
-}
